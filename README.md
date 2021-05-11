2b2246c8cf98d5cb68bfca214eeb11d84b984cb0
Ahmad Sakor <sakerobs@gmail.com>
2021-05-11 16:48:18 +0200

    first version

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-# CLARIFY_KG_Exploration_API
+# COVID-19-KG-Exploration-API
+
+# 1) Get Publication related to Drugs
+
+```
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{
+   "Drugs":[
+  	"C0031623",
+  	"C0751995",
+  	"C0030106"
+   ]
+}' \
+  https://labs.tib.eu/sdm/clarify-exp/kg-exp?target=Pub
+```
+
+# 2) Get Interactions of a Drug
+
+```
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{
+   "Drugs":[
+  	"C0000970",
+  	"C0028978",
+  	"C0009214"
+   ]
+}' \
+  https://labs.tib.eu/sdm/clarify-exp/kg-exp?target=DDI&limit=10&page=0
+```
+
+# 3) Get all the interaction among the provided Drugs
+
+
+```
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{
+   "Drugs":[
+  	"C0000970",
+  	"C0028978",
+  	"C0009214"
+   ]
+}' \
+  https://labs.tib.eu/sdm/clarify-exp/kg-exp?target=DDIS&limit=10&page=0
+```
+
+# 4) Get the predicted interactions of a Drug
+
+
+```
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{
+   "Drugs":[
+  	"C0000970",
+  	"C0028978",
+  	"C0009214"
+   ]
+}' \
+  https://labs.tib.eu/sdm/clarify-exp/kg-exp?target=DDIP&limit=10&page=0
+```
+
+# 5) Get all the predicted interaction among the provided Drugs
+
+
+```
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{
+   "Drugs":[
+    "C0013618",
+  	"C0010592",
+  	"C0043822"
+   ]
+}' \
+  https://labs.tib.eu/sdm/clarify-exp/kg-exp?target=DDIPS&limit=10&page=0
+```
---
+++ api.py
@@ -0,0 +1,304 @@
+#!/usr/bin/env python3
+#
+# Description: POST service for exploration of
+# data of Lung Cancer in the iASiS KG.
+#
+
+import sys
+from flask import Flask, abort, request, make_response , render_template
+import json
+from SPARQLWrapper import SPARQLWrapper, JSON
+import logging
+import os
+import itertools
+import get_publication
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+
+
+KG = os.environ["ENDPOINT"]
+#KG= 'http://node2.research.tib.eu:11282/sparql/'
+EMPTY_JSON = "{}"
+
+app = Flask(__name__)
+
+############################
+#
+# Query constants
+#
+############################
+
+
+
+
+QUERY_DRUG_TO_DRUGS_INTERACTIONS ="""
+SELECT DISTINCT ?effectorDrugLabel ?affectdDrugLabel ?adverse   WHERE {  
+                                           ?interaction <http://clarify2020.eu/vocab/precipitant_drug> ?effectorDrug.
+                                           ?interaction <http://clarify2020.eu/vocab/object_drug> ?affectdDrug.
+                                           ?affectdDrug <http://clarify2020.eu/vocab/hasCUIAnnotation> ?affectdDrugCUI.
+                                           ?effectorDrug <http://clarify2020.eu/vocab/drugLabel> ?effectorDrugLabel.
+                                           ?affectdDrug <http://clarify2020.eu/vocab/drugLabel> ?affectdDrugLabel.
+                                            ?interaction <http://clarify2020.eu/vocab/effect> ?adverse.                                 
+"""
+
+
+QUERY_DRUG_TO_DRUGS_INTERACTIONS_PREDICTED ="""
+SELECT DISTINCT ?effectorDrugLabel ?affectdDrugLabel ?confidence ?provenance WHERE {  
+                                           ?interaction a <http://clarify2020.eu/vocab/DrugDrugPrediction>.
+                                           ?interaction <http://clarify2020.eu/vocab/interactor1> ?effectorDrug.
+                                           ?interaction <http://clarify2020.eu/vocab/interactor2> ?affectdDrug.
+                                           ?affectdDrug <http://clarify2020.eu/vocab/hasCUIAnnotation> ?affectdDrugCUI.
+                                           ?effectorDrug <http://clarify2020.eu/vocab/drugLabel> ?effectorDrugLabel.
+                                           ?affectdDrug <http://clarify2020.eu/vocab/drugLabel> ?affectdDrugLabel.
+                                          ?interaction <http://clarify2020.eu/vocab/confidence> ?confidence.
+                                           ?interaction <http://clarify2020.eu/vocab/predictionMethod> ?provenance.                                
+"""
+
+QUERY_DRUGS_TO_DRUGS_INTERACTIONS ="""
+SELECT * {{
+{{SELECT DISTINCT ?effectorDrugLabel ?affectdDrugLabel ?adverse  WHERE {{
+                                           ?interaction <http://clarify2020.eu/vocab/precipitant_drug> ?effectorDrug.
+                                           ?interaction <http://clarify2020.eu/vocab/object_drug> ?affectdDrug.
+                                           ?effectorDrug <http://clarify2020.eu/vocab/hasCUIAnnotation> <http://clarify2020.eu/Annotation/{0}>.
+                                           ?affectdDrug <http://clarify2020.eu/vocab/hasCUIAnnotation> <http://clarify2020.eu/Annotation/{1}>.
+                                           ?effectorDrug <http://clarify2020.eu/vocab/drugLabel> ?effectorDrugLabel.
+                                           ?affectdDrug <http://clarify2020.eu/vocab/drugLabel> ?affectdDrugLabel.
+                                            ?interaction <http://clarify2020.eu/vocab/effect> ?adverse.   
+   
+}}}} UNION 
+{{SELECT DISTINCT ?effectorDrugLabel ?affectdDrugLabel ?adverse  WHERE {{  
+                                           ?interaction <http://clarify2020.eu/vocab/precipitant_drug> ?effectorDrug.
+                                           ?interaction <http://clarify2020.eu/vocab/object_drug> ?affectdDrug.
+                                           ?effectorDrug <http://clarify2020.eu/vocab/hasCUIAnnotation> <http://clarify2020.eu/Annotation/{1}>.
+                                           ?affectdDrug <http://clarify2020.eu/vocab/hasCUIAnnotation> <http://clarify2020.eu/Annotation/{0}>.
+                                           ?effectorDrug <http://clarify2020.eu/vocab/drugLabel> ?effectorDrugLabel.
+                                           ?affectdDrug <http://clarify2020.eu/vocab/drugLabel> ?affectdDrugLabel.
+                                            ?interaction <http://clarify2020.eu/vocab/effect> ?adverse.   
+}}}}}}                                     
+"""
+
+
+QUERY_DRUGS_TO_DRUGS_INTERACTIONS_PREDICTED ="""
+SELECT * {{
+{{SELECT DISTINCT ?effectorDrugLabel ?affectdDrugLabel ?confidence ?provenance  WHERE {{
+                                           ?interaction a <http://clarify2020.eu/vocab/DrugDrugPrediction>.
+                                           ?interaction <http://clarify2020.eu/vocab/interactor1> ?effectorDrug.
+                                           ?interaction <http://clarify2020.eu/vocab/interactor2> ?affectdDrug.
+                                           ?affectdDrug <http://clarify2020.eu/vocab/hasCUIAnnotation> <http://clarify2020.eu/Annotation/{0}>.
+                                           ?effectorDrug <http://clarify2020.eu/vocab/hasCUIAnnotation> <http://clarify2020.eu/Annotation/{1}>.
+                                           ?effectorDrug <http://clarify2020.eu/vocab/drugLabel> ?effectorDrugLabel.
+                                           ?affectdDrug <http://clarify2020.eu/vocab/drugLabel> ?affectdDrugLabel.
+                                          ?interaction <http://clarify2020.eu/vocab/confidence> ?confidence.
+                                           ?interaction <http://clarify2020.eu/vocab/predictionMethod> ?provenance.         
+}}}} UNION 
+{{SELECT DISTINCT ?effectorDrugLabel ?affectdDrugLabel ?confidence ?provenance  WHERE {{  
+                                           ?interaction a <http://clarify2020.eu/vocab/DrugDrugPrediction>.
+                                           ?interaction <http://clarify2020.eu/vocab/interactor1> ?effectorDrug.
+                                           ?interaction <http://clarify2020.eu/vocab/interactor2> ?affectdDrug.
+                                           ?affectdDrug <http://clarify2020.eu/vocab/hasCUIAnnotation> <http://clarify2020.eu/Annotation/{1}>.
+                                           ?effectorDrug <http://clarify2020.eu/vocab/hasCUIAnnotation> <http://clarify2020.eu/Annotation/{0}>.
+                                           ?effectorDrug <http://clarify2020.eu/vocab/drugLabel> ?effectorDrugLabel.
+                                           ?affectdDrug <http://clarify2020.eu/vocab/drugLabel> ?affectdDrugLabel.
+                                          ?interaction <http://clarify2020.eu/vocab/confidence> ?confidence.
+                                           ?interaction <http://clarify2020.eu/vocab/predictionMethod> ?provenance.   
+}}}}}}                                     
+"""
+############################
+#
+# Query generation
+#
+############################
+
+
+def execute_query(query,limit,page):
+    if limit!=0:
+       query+="LIMIT "+str(limit)
+    query+=" OFFSET "+str(page)   
+    sparql_ins = SPARQLWrapper(KG)
+    sparql_ins.setQuery(query)
+    sparql_ins.setReturnFormat(JSON)
+    return sparql_ins.query().convert()['results']['bindings']
+
+
+
+############################
+#
+# Processing results
+#
+############################
+
+def drug2_interactions_query(drug,limit,page):
+    query=QUERY_DRUG_TO_DRUGS_INTERACTIONS
+    query+="FILTER(?affectdDrugCUI in ("
+    query+="<http://clarify2020.eu/Annotation/"+drug+">"
+    query+="))}"
+        
+    qresults = execute_query(query,limit,page)
+    return qresults
+
+
+def drug2_interactions_predicted_query(drug,limit,page):
+    query=QUERY_DRUG_TO_DRUGS_INTERACTIONS_PREDICTED
+    query+="FILTER(?affectdDrugCUI in ("
+    query+="<http://clarify2020.eu/Annotation/"+drug+">"
+    query+="))}"
+        
+    qresults = execute_query(query,limit,page)
+    return qresults
+
+
+def drugs2_interactions_query(drug_pairs,limit,page):
+    query=QUERY_DRUGS_TO_DRUGS_INTERACTIONS.format(drug_pairs[0],drug_pairs[1])        
+    qresults = execute_query(query,limit,page)
+    return qresults
+
+
+def drugs2_interactions_predicted_query(drug_pairs,limit,page):
+    query=QUERY_DRUGS_TO_DRUGS_INTERACTIONS_PREDICTED.format(drug_pairs[0],drug_pairs[1])        
+    qresults = execute_query(query,limit,page)
+    return qresults
+
+
+def proccesing_response(input_dicc, target,limit,page,sort):
+    cuis=dict()
+    results=dict()
+
+    drugInteractions=dict()
+    for elem in input_dicc:
+        lcuis = input_dicc[elem]
+        if len(lcuis)==0:
+            continue
+        for item in lcuis:
+            cuis[item]=elem
+
+        if len(cuis)==0:
+            continue
+
+   
+       ############################Interactions#####################################         
+           
+        if elem=='Drugs':
+            if target=="DDI":
+                for drug in lcuis:
+                    query_reslut=drug2_interactions_query(drug,limit,page)
+                    drugInteractions[drug]=dict()
+                    if len(query_reslut)>0:
+                        drugInteractions[drug]["Label"]=query_reslut[0]["affectdDrugLabel"]["value"]
+                        drugInteractions[drug]["DDI"]=[]
+                        for result in query_reslut:
+                            interaction=dict()
+                            interaction["effectorDrug"]=result["effectorDrugLabel"]["value"]
+                            interaction["affectdDrug"]=result["affectdDrugLabel"]["value"]
+                            interaction["effect"]=result["adverse"]["value"].replace('http://clarify2020.eu/AdverseEvent/','').split('_')[0]
+                            interaction["impact"]=result["adverse"]["value"].replace('http://clarify2020.eu/AdverseEvent/','').split('_')[1]
+                            drugInteractions[drug]["DDI"].append(interaction)
+            elif target=="DDIS":
+                drugs_pairs=[(x,y) for x,y in list(itertools.product(lcuis, lcuis)) if x!=y and x<y]
+                for drug_pair in drugs_pairs :
+                    query_reslut=drugs2_interactions_query(drug_pair,limit,page)
+                    drugInteractions[str(drug_pair)]=dict()
+                    if len(query_reslut)>0:
+                        drugInteractions[str(drug_pair)]["Labels"]=query_reslut[0]["affectdDrugLabel"]["value"]+" AND "+query_reslut[0]["effectorDrugLabel"]["value"]
+                        drugInteractions[str(drug_pair)]["DDIS"]=[]
+                        for result in query_reslut:
+                            interaction=dict()
+                            interaction["effectorDrug"]=result["effectorDrugLabel"]["value"]
+                            interaction["affectdDrug"]=result["affectdDrugLabel"]["value"]
+                            interaction["effect"]=result["adverse"]["value"].replace('http://clarify2020.eu/AdverseEvent/','').split('_')[0]
+                            interaction["impact"]=result["adverse"]["value"].replace('http://clarify2020.eu/AdverseEvent/','').split('_')[1]
+                            if interaction not in drugInteractions[str(drug_pair)]["DDIS"]:
+                                drugInteractions[str(drug_pair)]["DDIS"].append(interaction)
+            elif target=="DDIP":
+                #drugs_pairs=[(x,y) for x,y in list(itertools.product(lcuis, lcuis)) if x!=y]
+                for drug in lcuis:
+                    query_reslut=drug2_interactions_predicted_query(drug,limit,page)
+                    drugInteractions[drug]=dict()
+                    if len(query_reslut)>0:
+                        drugInteractions[drug]["Label"]=query_reslut[0]["affectdDrugLabel"]["value"]
+                        drugInteractions[drug]["DDIP"]=[]
+                        for result in query_reslut:
+                            interaction=dict()
+                            interaction["effectorDrug"]=result["effectorDrugLabel"]["value"]
+                            interaction["affectdDrug"]=result["affectdDrugLabel"]["value"]
+                            interaction["confidence"]=result["confidence"]["value"]
+                            interaction["provenance"]=result["provenance"]["value"]
+                            drugInteractions[drug]["DDIP"].append(interaction)
+            elif target=="DDIPS":
+                drugs_pairs=[(x,y) for x,y in list(itertools.product(lcuis, lcuis)) if x!=y and x<y]
+                for drug_pair in drugs_pairs :
+                    query_reslut=drugs2_interactions_predicted_query(drug_pair,limit,page)
+                    drugInteractions[str(drug_pair)]=dict()
+                    if len(query_reslut)>0:
+                        drugInteractions[str(drug_pair)]["Labels"]=query_reslut[0]["affectdDrugLabel"]["value"]+" AND "+query_reslut[0]["effectorDrugLabel"]["value"]
+                        drugInteractions[str(drug_pair)]["DDIPS"]=[]
+                        for result in query_reslut:
+                            interaction=dict()
+                            interaction["effectorDrug"]=result["effectorDrugLabel"]["value"]
+                            interaction["affectdDrug"]=result["affectdDrugLabel"]["value"]
+                            interaction["confidence"]=result["confidence"]["value"]
+                            interaction["provenance"]=result["provenance"]["value"]
+                            drugInteractions[str(drug_pair)]["DDIPS"].append(interaction)
+
+        
+    results['Interactions']=drugInteractions
+    return results
+           
+
+
+
+
+
+
+@app.route('/kg-exp', methods=['POST'])
+def run_exploration_api():
+    if (not request.json):
+        abort(400)
+    if 'limit' in request.args:
+        limit = int(request.args['limit'])
+    else:
+        limit = 0
+    if 'page' in request.args:
+        page = int(request.args['page'])
+    else:
+        page = 0
+    if 'sort' in request.args:
+        sort = request.args['sort']
+    else:
+        sort = 0
+    if 'target' in request.args:
+        target = request.args['target']
+    else:
+        abort(400)
+
+
+    input_list = request.json
+    if len(input_list) == 0:
+        logger.info("Error in the input format")
+        r = "{results: 'Error in the input format'}"
+    else:
+        if target!="Pub":
+            response = proccesing_response(input_list,target,limit,page,sort)       
+        elif target=="Pub":
+            response=get_publication.process(input_list,KG)
+    r = json.dumps(response, indent=4)  
+    logger.info("Sending the results: ")
+    response = make_response(r, 200)
+    response.mimetype = "application/json"
+    return response
+
+@app.route('/')
+def render_static_home():
+    return render_template('index.html')  
+
+def main(*args):
+    if len(args) == 1:
+        myhost = args[0]
+    else:
+        myhost = "0.0.0.0"
+    app.run(debug=False, host=myhost)
+    
+if __name__ == '__main__':
+     main(*sys.argv[1:])
